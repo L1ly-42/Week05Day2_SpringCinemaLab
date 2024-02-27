fd31675a6577fe86d62f7f67dcc669ccf534d391
Leila Peltier <l_peltier@outlook.com>
2024-02-27 19:37:49 +0000

    Movie model created

---
+++ cinema/src/main/java/com/example/models/Movie.java
@@ -0,0 +1,55 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="title")
+    private String title;
+
+    @Column(name ="rating")
+    private String rating;
+
+    @Column(name ="duration")
+    private int duration;
+
+    public Movie(){}
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
