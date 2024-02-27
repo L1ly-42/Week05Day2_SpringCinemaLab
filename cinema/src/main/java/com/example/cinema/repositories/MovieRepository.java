3aa2fe3060aabf8bac02fd71b17bcbc516ced3e2
Leila Peltier <l_peltier@outlook.com>
2024-02-27 19:45:57 +0000

    Created Movie Repository

---
+++ cinema/src/main/java/com/example/cinema/repositories/MovieRepository.java
@@ -0,0 +1,8 @@
+
+
+package com.example.cinema.repositories;
+import com.example.cinema.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
