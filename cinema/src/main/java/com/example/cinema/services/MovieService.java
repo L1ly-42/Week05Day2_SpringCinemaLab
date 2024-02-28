c8c665e19de95e1133c4cd99a3be06ed5028f0af
Leila Peltier <l_peltier@outlook.com>
2024-02-28 01:16:47 +0000

    Created MovieService

---
+++ cinema/src/main/java/com/example/cinema/services/MovieService.java
@@ -0,0 +1,24 @@
+package com.example.cinema.services;
+
+import com.example.cinema.models.Movie;
+import com.example.cinema.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MovieService{
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    public MovieService(){}
+
+    public Optional<Movie> getMovieById(long id) {
+        return movieRepository.findById(id);
+    }
+
+
+
+}
